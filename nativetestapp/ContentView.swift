559ead47ccf93c5da711b3de91e3a4bff3823361
mrk-nvr <mark@nevercode.io>
2021-02-11 16:11:44 +0200

    added initial app

---
+++ nativetestapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nativetestapp
+//
+//  Created by Codemagic PC on 11.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nativetestapp/nativetestappApp.swift
@@ -0,0 +1,17 @@
+//
+//  nativetestappApp.swift
+//  nativetestapp
+//
+//  Created by Codemagic PC on 11.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct nativetestappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
